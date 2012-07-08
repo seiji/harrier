73ebcdf96d63d88712b68ae3659f87db5e08ea18
Seiji Toyama <seijit@me.com>
2012-07-08 18:52:27 +0900

    Add twitter_amazon

--- scripts/twitter/twitter_amazon.rb
+++ scripts/twitter/twitter_amazon.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 require File.dirname(__FILE__) + './config'
 
-TRACK="itunes"
-DB_NAME="itunes"
+TRACK="amazon"
+DB_NAME="amazon"
 
 if $0 == __FILE__  
   `curl https://stream.twitter.com/1/statuses/filter.json?track=#{TRACK} -u#{TWITTER_CONFIG['user']}:#{TWITTER_CONFIG['pass']} | #{MONGO_HOME}/bin/mongoimport -d twitter -c #{DB_NAME}`
