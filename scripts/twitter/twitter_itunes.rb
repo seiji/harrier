2886574a6cda78590e50a8c6ccde3165f6012b0b
Seiji Toyama <seijit@me.com>
2012-07-08 16:19:18 +0900

    Edit runscript

--- scripts/twitter/twitter_itunes.rb
+++ scripts/twitter/twitter_itunes.rb
@@ -3,11 +3,12 @@ require 'pit'
 TWITTER_CONFIG = Pit.get('twitter.com')
 TRACK="itunes"
 DB_NAME="itunes"
+MONGO_HOME="/usr/local/mongodb"
 
 if $0 == __FILE__
   unless TWITTER_CONFIG
     raise "u set TWITTER_CONFIG"
   end
   
-  `curl https://stream.twitter.com/1/statuses/filter.json?track=#{TRACK} -u#{TWITTER_CONFIG['user']}:#{TWITTER_CONFIG['pass']} | mongoimport -d twitter -c #{DB_NAME}`
+  `curl https://stream.twitter.com/1/statuses/filter.json?track=#{TRACK} -u#{TWITTER_CONFIG['user']}:#{TWITTER_CONFIG['pass']} | #{MONGO_HOME}/bin/mongoimport -d twitter -c #{DB_NAME}`
 end
